f132518bc496f2b495680887d39f09c986377c1a
Quanmuito <49555971+Quanmuito@users.noreply.github.com>
2020-09-06 10:04:50 +0300

    Updates

---
+++ resources/views/heroes/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-12">
+        <h1>Hello from create page</h1>
+    </div>
+</div>
+@endsection
