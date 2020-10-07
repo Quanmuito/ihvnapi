e4a3c51c70f9703408c0abdd27ab8b8a7e955a81
Quanmuito <49555971+Quanmuito@users.noreply.github.com>
2020-10-07 19:55:23 +0300

    Updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <h1>Một số lưu ý khi dịch</h1>
+    <ul>
+        <li>Không viết tắt, nhớ chú ý dấu câu</li>
+        <li>Phần ảnh thì nhập đúng tên hiển thị trên Google Drive</li>
+        <li>Sau khi submit nhớ check lại xem ảnh có hiển thị không, nội dung có bị sai không</li>
+        <li>Kiểm tra kĩ số sao (star) và faction trước khi submit</li>
+    </ul>
 @endsection
